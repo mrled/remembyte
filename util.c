e8a9f9b6f7230f38695ad4aa59e6b99a2aac34e4
Micah R Ledbetter <mrled@younix.us>
2015-01-08 00:26:03 -0600

    Improve util: dbgprintf(), str2bool(), resolve_path()
    - Prefix dbgprintf() output with "DBG: "
    - Add str2bool() for more easily reading config file
    - Add resolve_path(), currently POSIX-only

--- util.c
+++ util.c
@@ -2,10 +2,18 @@
 
 void dbgprintf(const char *format, ...) {
   if (DEBUGMODE) {
+    char *format2, *prefix;
+    prefix = "DBG: ";
+    format2 = malloc(strlen(format2) + strlen(format) +1);
+    memcpy(format2, prefix, strlen(prefix));
+    memcpy(format2 +strlen(prefix), format, strlen(format)+1);
+
     va_list args;
-    va_start(args, format);
-    vfprintf(stderr, format, args);
+    va_start(args, format2);
+    vfprintf(stderr, format2, args);
     va_end(args);
+
+    free(format2);
   }
 }
 
@@ -24,6 +32,62 @@ bool safe_strncmp(const char * str1, const char * str2, size_t len) {
 bool safe_strcmp(const char * str1, const char * str2) {
   if (strlen(str1) == strlen(str2)) {
     return safe_strncmp(str1, str2, strlen(str1));
+  }
+  else {
+    return false;
+  }
+}
+
+
+/* Of course the fucking standard doesn't have a cross-platform way to resolve
+ * a fucking path
+ * 
+ * Take a path that might contain ., .., extraneous /, or a ~ character, and 
+ * return the absolute path that the user intends
+ */
+char * resolve_path(const char * path) {
+  char *resolved, *query;
+
+  query = strdup(path);
+
+  // Check for ~
+  if (path[0] == '~') {
+    char *home;
+    home = getenv("HOME");
+    if (!home) {
+      fprintf(stderr, "Could not find HOME variable\n");
+      return NULL;
+    }
+
+    if (strlen(path) == 1) {
+      // We were passed just a tilde and nothing else
+      return home;
+    }
+    else if ((path[1] == '/') || (path[1] == '\\')) {
+      // We were passed a tilde and then a path seperator 
+      // such as "~/file.txt"
+      query = strcat(home, &path[1]);
+    }
+    else {
+      // We were passed a tilde and then something that wasn't a path separator
+      // such as "~username/file.txt"
+      fprintf(stderr, "Error for input '%s': '~username' syntax is currently "
+        "unsupported\n", path);
+      return NULL;
+    }
+  }
+
+  // TODO: this isn't cross platform yet
+  resolved = realpath(query, NULL);
+
+  return resolved;
+}
+
+bool str2bool(char *str) {
+  long strl;
+  strl = strtol(str, NULL, 10);
+  if (safe_strcmp(str, "true") || safe_strcmp(str, "True") || strl) {
+    return true;
   }
   else {
     return false;
--- util.h
+++ util.h
@@ -6,11 +6,14 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 extern bool DEBUGMODE;
 
 void dbgprintf(const char *, ...);
 bool safe_strcmp(const char *, const char *);
 bool safe_strncmp(const char *, const char *, size_t);
+char * resolve_path(const char *);
+bool str2bool(char *);
 
 #endif
